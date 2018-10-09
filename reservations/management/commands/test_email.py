30de7defdf396a7bbf63d28883ac0bd99c1b653c
Sylvain Picard <sylvain.picard@chu-lyon.Fr>
2018-10-09 15:48:23 +0200

    Ajout commande pour tester fonctionnement email

---
+++ reservations/management/commands/test_email.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand, CommandError
+from reservations.models import Creneau, Cours
+from datetime import datetime, timedelta
+from django.core.mail import send_mail
+
+
+class Command(BaseCommand):
+    help = "Test envoi de mail via smtp"
+
+    def handle(self, *args, **options):
+        from_email="picard.sylvain3@gmail.com"
+        to = ["picard.sylvain3@gmail.com"]
+        subject = "Aquabike test email"
+        message = "Working email system"
+        send_mail(subject, message, from_email, to)
